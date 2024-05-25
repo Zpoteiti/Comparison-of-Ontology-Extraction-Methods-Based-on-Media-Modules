3473a9f433ebf4b1e4c705a5f6382a3848df2459
Zpoteiti <zacharyzou.yc@outlook.com>
2024-05-25 13:02:53 +0200

    Initial commit

---
+++ Yucheng Zou/convert_sig_ForMod.py
@@ -0,0 +1,42 @@
+import os
+
+def extract_urls(text):
+    urls = []
+    start = text.find("[") + 1
+    end = text.find("]")
+    urls_text = text[start:end]
+    urls = urls_text.split("\n")
+    urls = [url.strip() for url in urls if url.strip()]
+    return urls
+
+def format_urls(urls):
+    return ' '.join(urls)
+
+if __name__ == "__main__":
+    # Specify the path where the signature files are located
+    signature_folder = r"C:\thesis\Signatures_nci16\sig_50_1"
+
+    # Specify the path for the output folder
+    output_folder = r"C:\thesis\formod-main\workspace\nci-16\sig"
+
+    # Loop through each signature file in the specified directory
+    for file_name in os.listdir(signature_folder):
+        input_file = os.path.join(signature_folder, file_name)
+        output_file = os.path.join(output_folder, f"{file_name}")
+
+        # Reading input from the signature file
+        with open(input_file, "r") as file:
+            input_text = file.read()
+
+        classes_text, roles_text = input_text.split("Roles[")
+
+        classes_urls = extract_urls(classes_text)
+        formatted_classes = format_urls(classes_urls)
+
+        roles_urls = extract_urls(roles_text)
+        formatted_roles = format_urls(roles_urls)
+
+        # Writing output to a new file in the output folder
+        with open(output_file, "w") as file:
+            file.write(formatted_classes + "\n")
+            file.write(formatted_roles)

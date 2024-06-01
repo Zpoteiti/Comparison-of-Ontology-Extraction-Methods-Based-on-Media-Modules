c66e3d7871fdae0f1eb8097a034ab0e5a1c5ab6d
Yucheng Zou <zacharyzou.yc@outlook.com>
2024-06-01 11:58:52 +0200

    Updated sig translater mex

---
+++ Yucheng Zou/run_mex.py
@@ -0,0 +1,2 @@
+"sample command for running the mex file:"
+"yz@yz-MS-Terminator-B760ITX-D4:~/thesis$ ./mex.bin -tbx /home/yz/formod/workspace/nci-16/nci-16.krss.owl -sig /home/yz/thesis/sig_MEX/sig_50_1/0"
